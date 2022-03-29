a5ab16264a1482aeafe56cfafc9d84e6b89957eb
pratikagupta <pratika.gupta008@gmail.com>
2022-03-29 19:00:03 +0530

    Updated APIs

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -10,6 +10,8 @@ import Foundation
 struct Constants {
     static let API_KEY = "9653284883c57b8d3d92678469896bc5"
     static let baseURL = "https://api.themoviedb.org"
+    static let YoutubeAPI_KEY = "AIzaSyBIP5ACoPCkM-uJ-IPsPU6VatBlEK6lszc"
+    static let YoutubeBaseURL = "GET https://youtube.googleapis.com/youtube/v3/search"
 }
 
 enum APIError: Error {
@@ -105,7 +107,67 @@ class APICaller {
     }
         task.resume()
     }
+    
+    func getDiscoverMovies(completion: @escaping (Result<[Title], Error>) -> Void){
+        guard let url = URL(string: "\(Constants.baseURL)/3/discover/movie?api_key=\(Constants.API_KEY)&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate") else {return}
+        let task = URLSession.shared.dataTask(with:   URLRequest(url:url)) { data, _, error in
+        guard let data = data, error == nil else {
+                    return
+        }
+        do {
+            let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+            completion(.success(results.results))
+        } catch {
+            completion(.failure(APIError.failedTogetData))
+        }
+    }
+        task.resume()
+    }
+    
+    
+    
+    func search(with query: String, completion: @escaping (Result<[Title], Error>) -> Void){
+        
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {return}
+        
+        guard let url = URL(string: "\(Constants.baseURL)/3/search/movie?api_key=\(Constants.API_KEY)&query=\(query)") else {return}
+        let task = URLSession.shared.dataTask(with:   URLRequest(url:url)) { data, _, error in
+        guard let data = data, error == nil else {
+                    return
+        }
+        do {
+            let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+            completion(.success(results.results))
+        } catch {
+            completion(.failure(APIError.failedTogetData))
+        }
+    }
+        task.resume()
+    }
+    
+    
+    func getMovie(with query: String){
+        
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {return}
+        guard let url = URL(string: "\(Constants.YoutubeBaseURL)q=\(query)&key=\(Constants.YoutubeAPI_KEY)") else
+             {return}
+        let task = URLSession.shared.dataTask(with: URLRequest(url:url)) { data, _, error in
+        guard let data = data, error == nil else {
+                    return
+        }
+        do {
+            let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+            print(results)
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+        task.resume()
+    }
 }
+    
+
 
 
 
---
+++ Netflix Clone/ViewsModels/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Pratika Gupta on 28/03/22.
+//
+
+import Foundation
+
+
+struct TitleViewModel{
+    let titleName: String
+    let posterURL: String
+}
