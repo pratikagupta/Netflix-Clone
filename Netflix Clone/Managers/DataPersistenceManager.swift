5508490592c946e42a6a96c2328612dda7eb4b1c
pratikagupta <pratika.gupta008@gmail.com>
2022-04-16 20:09:18 +0530

    Update download view section

--- Netflix Clone/Managers/DataPersistenceManager.swift
+++ Netflix Clone/Managers/DataPersistenceManager.swift
@@ -7,19 +7,21 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class DataPersistenceManager {
     
-    
     enum DatabaseError: Error {
         case failedToSaveData
+        case failedToFetchData
+        case failedToDeleteData
     }
     
-    static let shared = DataPersistenceManager ()
+    static let shared = DataPersistenceManager()
     
     
     func downloadTitleWith(model: Title, completion: @escaping (Result<Void, Error>) -> Void) {
-        
+         
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
         }
@@ -40,11 +42,52 @@ class DataPersistenceManager {
         
         do {
             try context.save()
-            completion(.success(()))
+              completion(.success(()))
         } catch {
             completion(.failure(DatabaseError.failedToSaveData))
         }
     }
     
+    func fetchingTitlesFromDataBase(completion: @escaping (Result<[TitleItem], Error>) -> Void){
     
+    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+        return
+    }
+    
+    let context = appDelegate.persistentContainer.viewContext
+        
+        let request: NSFetchRequest<TitleItem>
+        
+        request = TitleItem.fetchRequest()
+        
+        do {
+            
+            let titles = try context.fetch(request)
+            completion(.success(titles))
+            
+        } catch {
+            completion(.failure(DatabaseError.failedToFetchData))
+            
+        }
+    }
+    
+    
+    func deleteTitleWith(model: TitleItem, completion: @escaping (Result<Void, Error>)-> Void){
+     
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        context.delete(model) //asking the database manager to delete certain object
+        
+        do {
+            try context.save()
+            completion(.success(()))
+        } catch {
+            completion(.failure(DatabaseError.failedToDeleteData))
+        }
+        
+    }
 }
--- Netflix Clone/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -59,7 +59,7 @@ class CollectionViewTableViewCell: UITableViewCell {
         DataPersistenceManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
             switch result {
             case.success():
-                print("downloaded to Database")
+                NotificationCenter.default.post(name: NSNotification.Name("downloaded"), object: nil)
             case.failure(let error):
                 print(error.localizedDescription)
             }
@@ -134,6 +134,7 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
         
         return config
     }
+    
 }
     
 
